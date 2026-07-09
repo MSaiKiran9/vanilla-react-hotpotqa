04dfa2743bd8f9249675dada598753bdb29f6c26
sai <msaikiran9848250763@gmail.com>
2026-07-09 23:38:22 +0530

    save to drive=true

--- config.py
+++ config.py
@@ -123,7 +123,7 @@ OUTPUT_FIGURE = OUTPUT_DIR / "model_comparison.png"
 
 TRACE_LOG_DIR = LOG_DIR
 
-SAVE_TO_DRIVE = False
+SAVE_TO_DRIVE = True
 
 DRIVE_OUTPUT_DIR = "/content/drive/MyDrive/VanillaReActResults"
 
